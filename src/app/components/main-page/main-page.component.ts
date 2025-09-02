b9e384523fbe19ca8d892e6fe6b46c66b8d10055
Thesasan1 <sasanshirdel.mail@gmail.com>
2025-09-02 13:06:08 +0330

    finished parallax section

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { HomeSectionComponent } from "../home-section/home-section.component";
 import { HeroSectionComponent } from "../hero-section/hero-section.component";
+import { ParallaxSectionComponent } from "../parallax-section/parallax-section.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent, HomeSectionComponent, HeroSectionComponent],
+  imports: [HeaderComponent, HomeSectionComponent, HeroSectionComponent, ParallaxSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
---
+++ src/app/components/parallax-section/parallax-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+
+@Component({
+  selector: 'app-parallax-section',
+  imports: [ButtonComponent],
+  templateUrl: './parallax-section.component.html',
+  styleUrl: './parallax-section.component.css'
+})
+export class ParallaxSectionComponent {
+
+}
