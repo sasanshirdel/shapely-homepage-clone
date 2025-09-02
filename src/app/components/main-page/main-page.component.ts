01002094c2d2a8c508c9d033405c5d53ec9abf87
Thesasan1 <sasanshirdel.mail@gmail.com>
2025-09-02 13:14:30 +0330

    finished options section

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "../header/header.component";
 import { HomeSectionComponent } from "../home-section/home-section.component";
 import { HeroSectionComponent } from "../hero-section/hero-section.component";
 import { ParallaxSectionComponent } from "../parallax-section/parallax-section.component";
+import { OptionsSectionComponent } from "../options-section/options-section.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent, HomeSectionComponent, HeroSectionComponent, ParallaxSectionComponent],
+  imports: [HeaderComponent, HomeSectionComponent, HeroSectionComponent, ParallaxSectionComponent, OptionsSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
---
+++ src/app/components/options-section/options-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+
+@Component({
+  selector: 'app-options-section',
+  imports: [ButtonComponent],
+  templateUrl: './options-section.component.html',
+  styleUrl: './options-section.component.css'
+})
+export class OptionsSectionComponent {
+
+}
